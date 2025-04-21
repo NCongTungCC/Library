2826ae7e4bdbb3114de0e20dad3d123c2fb92f01
congtung <tung9x9z123@gmail.com>
2025-04-21 16:52:50 +0700

    update

--- src/server.js
+++ src/server.js
@@ -40,6 +40,7 @@ const app = http.createServer((req, res) => {
     else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('Not Found');
+        return;
     }
 });
 
