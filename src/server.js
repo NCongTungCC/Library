1b0dcec06c995c3ef4cabfeeabcaa7bf2d31b2fc
congtung <tung9x9z123@gmail.com>
2025-04-21 15:20:07 +0700

    server

--- src/server.js
+++ src/server.js
@@ -1,10 +1,46 @@
-const express = require('express');
-const app = express();
 const port = 3000;
-
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
+const http = require('http');
+const app = http.createServer((req, res) => {
+    const { method, url } = req;
+    if(method === 'GET' && url === '/') {
+        res.end("Hello World");
+        return;
+    }
+    if(method === 'POST' && url === '/data') {
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString(); 
+        });
+        req.on('end', () => {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ 
+                message: 'Tạo thành công', 
+                data: JSON.parse(body) }));
+        });
+        return;
+    } 
+    if(method === 'PUT' && url === '/data') {
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString(); 
+        });
+        req.on('end', () => {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ 
+                message: 'Cập nhật thành công', 
+                data: JSON.parse(body) }));
+        });
+        return;
+    }
+    if(method === 'DELETE' && url === '/data') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'Xóa' }));
+        return;
+    }
+    else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
 });
 
 app.listen(port, () => {
